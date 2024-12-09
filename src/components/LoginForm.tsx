5725333d525eeff3785ee95be2d20dd649e165f9
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-12-09 18:02:44 -0300

    feat: I commented the span line that had the, Or continue with

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -34,7 +34,7 @@ const LoginForm = () => {
                             <div className="w-full border-t border-gray-300"></div>
                         </div>
                         <div className="relative flex justify-center text-sm">
-                            <span className="px-2 bg-white text-gray-500">Ou continue com</span>
+                           {/*<span className="px-2 bg-white text-gray-500">Ou continue com</span>*/} 
                         </div>
                     </div>
                     <div className="mt-6 grid grid-cols-1 justify-items-center items-center ">
